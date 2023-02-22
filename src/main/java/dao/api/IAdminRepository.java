af2ada168cec955b1f753f61226e0c7fe8dc39db
MaxStribuk <maximus_747@mail.ru>
2023-02-22 18:45:21 +0300

    update adminDao

--- src/main/java/dao/api/IAdminRepository.java
+++ src/main/java/dao/api/IAdminRepository.java
@@ -3,19 +3,16 @@ package dao.api;
 import dao.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface IAdminDao extends PagingAndSortingRepository<UserEntity, UUID> {
+public interface IAdminRepository extends Repository<UserEntity, UUID> {
 
-    @Override
-    <S extends UserEntity> S save(S entity);
+    void save(UserEntity user);
 
-    @Override
     Optional<UserEntity> findById(UUID uuid);
 
-    @Override
     Page<UserEntity> findAll(Pageable pageable);
 }
--- src/main/java/service/impl/AdminService.java
+++ src/main/java/service/impl/AdminService.java
@@ -3,7 +3,7 @@ package service.impl;
 import core.dto.request.UserCreateDto;
 import core.dto.response.PageUserDto;
 import core.dto.response.PageDto;
-import dao.api.IAdminDao;
+import dao.api.IAdminRepository;
 import dao.entity.UserEntity;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.domain.Page;
@@ -17,16 +17,16 @@ import java.util.UUID;
 
 public class AdminService implements IAdminService {
 
-    private final IAdminDao adminDao;
+    private final IAdminRepository adminRepository;
     private final Converter<UserCreateDto, UserEntity> userCreateDtoEntityConverter;
     private final Converter<UserEntity, PageUserDto> userEntityPageDtoConverter;
     private final IConverter<UserEntity, PageUserDto> entityPageDtoConverter;
 
-    public AdminService(IAdminDao adminDao,
+    public AdminService(IAdminRepository adminRepository,
                         Converter<UserCreateDto, UserEntity> userCreateDtoEntityConverter,
                         Converter<UserEntity, PageUserDto> userEntityPageDtoConverter,
                         IConverter<UserEntity, PageUserDto> entityPageDtoConverter) {
-        this.adminDao = adminDao;
+        this.adminRepository = adminRepository;
         this.userCreateDtoEntityConverter = userCreateDtoEntityConverter;
         this.userEntityPageDtoConverter = userEntityPageDtoConverter;
         this.entityPageDtoConverter = entityPageDtoConverter;
@@ -35,31 +35,31 @@ public class AdminService implements IAdminService {
     @Override
     public void add(UserCreateDto user) {
         UserEntity userEntity = userCreateDtoEntityConverter.convert(user);
-        adminDao.save(userEntity);
+        adminRepository.save(userEntity);
     }
 
     @Override
     public PageDto<PageUserDto> getAll(Pageable pageable) {
-        Page<UserEntity> users = adminDao.findAll(pageable);
+        Page<UserEntity> users = adminRepository.findAll(pageable);
         return entityPageDtoConverter.convert(users, userEntityPageDtoConverter);
     }
 
     @Override
     public PageUserDto get(UUID uuid) {
-        Optional<UserEntity> userEntityOptional = adminDao.findById(uuid);
+        Optional<UserEntity> userEntityOptional = adminRepository.findById(uuid);
         UserEntity userEntity = userEntityOptional.orElseThrow(IllegalArgumentException::new);
         return userEntityPageDtoConverter.convert(userEntity);
     }
 
     @Override
     public void update(UUID uuid, int dtUpdate, UserCreateDto user) {
-        Optional<UserEntity> userEntityOptional = adminDao.findById(uuid);
+        Optional<UserEntity> userEntityOptional = adminRepository.findById(uuid);
         UserEntity userEntity = userEntityOptional.orElseThrow(IllegalArgumentException::new);
         if (dtUpdate != userEntity.getDtUpdate().toEpochSecond(ZoneOffset.UTC)) {
             throw new IllegalArgumentException();
         }
         update(userEntity, user);
-        adminDao.save(userEntity);
+        adminRepository.save(userEntity);
     }
 
     private void update(UserEntity userEntity, UserCreateDto user) {
