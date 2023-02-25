a54225db9043eff0d3775a3311f24f3789893bcb
MaxStribuk <maximus_747@mail.ru>
2023-02-25 15:44:23 +0300

    refactoring

--- src/main/java/by/itacademy/service/util/converters/EntityPageDtoConverter.java
+++ src/main/java/by/itacademy/service/util/converters/EntityPageDtoConverter.java
@@ -6,14 +6,15 @@ import by.itacademy.repository.entity.UserEntity;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.domain.Page;
 import by.itacademy.service.api.IConverter;
+import org.springframework.lang.NonNull;
 
 import java.util.stream.Collectors;
 
 public class EntityPageDtoConverter implements IConverter<UserEntity, PageUserDto> {
 
     @Override
-    public PageDto<PageUserDto> convert(Page<UserEntity> source,
-                                        Converter<UserEntity, PageUserDto> converter) {
+    public PageDto<PageUserDto> convert(@NonNull Page<UserEntity> source,
+                                        @NonNull Converter<UserEntity, PageUserDto> converter) {
         return new PageDto<>(
                 source.getNumber(),
                 source.getSize(),
--- src/main/java/by/itacademy/service/util/converters/UserEntityPageDtoConverter.java
+++ src/main/java/by/itacademy/service/util/converters/UserEntityPageDtoConverter.java
@@ -3,11 +3,12 @@ package by.itacademy.service.util.converters;
 import by.itacademy.core.dto.response.PageUserDto;
 import by.itacademy.repository.entity.UserEntity;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 public class UserEntityPageDtoConverter implements Converter<UserEntity, PageUserDto> {
 
     @Override
-    public PageUserDto convert(UserEntity user) {
+    public PageUserDto convert(@NonNull UserEntity user) {
         return new PageUserDto(
                 user.getUuid(),
                 user.getDtCreate(),
