93f33110e1f629875bb9ad802edec515625ea95a
MaxStribuk <maximus_747@mail.ru>
2023-03-02 19:30:18 +0300

    create ProductEntity

---
+++ src/main/java/by/itacademy/repository/entity/ProductEntity.java
@@ -0,0 +1,167 @@
+package by.itacademy.repository.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(schema = "app", name = "products")
+public class ProductEntity implements Serializable {
+
+    @Id
+    @GeneratedValue()
+    @Type(type = "pg-uuid")
+    @Column(name = "uuid", nullable = false, length = 36)
+    private UUID uuid;
+
+    @Column(name = "dt_create", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime dtCreate;
+
+    @Column(name = "dt_update", nullable = false)
+    @Version
+    private LocalDateTime dtUpdate;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "weight", nullable = false)
+    private int weight;
+
+    @Column(name = "calories", nullable = false)
+    private int calories;
+
+    @Column(name = "proteins", nullable = false)
+    private double proteins;
+
+    @Column(name = "fats", nullable = false)
+    private double fats;
+
+    @Column(name = "carbohydrates", nullable = false)
+    private double carbohydrates;
+
+    public ProductEntity() {
+    }
+
+    public ProductEntity(UUID uuid, LocalDateTime dtCreate,
+                         LocalDateTime dtUpdate, String title,
+                         int weight, int calories, double proteins,
+                         double fats, double carbohydrates) {
+        this.uuid = uuid;
+        this.dtCreate = dtCreate;
+        this.dtUpdate = dtUpdate;
+        this.title = title;
+        this.weight = weight;
+        this.calories = calories;
+        this.proteins = proteins;
+        this.fats = fats;
+        this.carbohydrates = carbohydrates;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public LocalDateTime getDtCreate() {
+        return dtCreate;
+    }
+
+    public LocalDateTime getDtUpdate() {
+        return dtUpdate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public double getProteins() {
+        return proteins;
+    }
+
+    public double getFats() {
+        return fats;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void setProteins(double proteins) {
+        this.proteins = proteins;
+    }
+
+    public void setFats(double fats) {
+        this.fats = fats;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductEntity that = (ProductEntity) o;
+        return weight == that.weight
+                && calories == that.calories
+                && Double.compare(that.proteins, proteins) == 0
+                && Double.compare(that.fats, fats) == 0
+                && Double.compare(that.carbohydrates, carbohydrates) == 0
+                && Objects.equals(uuid, that.uuid)
+                && Objects.equals(dtCreate, that.dtCreate)
+                && Objects.equals(dtUpdate, that.dtUpdate)
+                && Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, dtCreate, dtUpdate, title,
+                weight, calories, proteins, fats, carbohydrates);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEntity{" +
+                "uuid=" + uuid +
+                ", dtCreate=" + dtCreate +
+                ", dtUpdate=" + dtUpdate +
+                ", title='" + title + '\'' +
+                ", weight=" + weight +
+                ", calories=" + calories +
+                ", proteins=" + proteins +
+                ", fats=" + fats +
+                ", carbohydrates=" + carbohydrates +
+                '}';
+    }
+}
