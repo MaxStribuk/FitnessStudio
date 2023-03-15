c97de144e129ab4e98eba217ab11a389e847466f
MaxStribuk <maximus_747@mail.ru>
2023-03-15 15:44:57 +0300

    add dml default admin

--- postgres/dml/00.add_all.sql
+++ postgres/dml/00.add_all.sql
@@ -9,3 +9,13 @@ INSERT INTO app.mail_status(status) VALUES ('WAITING');
 INSERT INTO app.mail_status(status) VALUES ('SENT');
 INSERT INTO app.mail_status(status) VALUES ('ERROR');
 INSERT INTO app.mail_status(status) VALUES ('SUCCESS');
+
+INSERT INTO app.users
+VALUES ('00000000-0000-0000-0000-000000000000',
+        '2023-01-01 00:00:00.000',
+        '2023-01-01 00:00:00.000',
+        'maximus747@mail.ru',
+        'Max',
+        2,
+        1,
+        '$2a$10$WzPoFH6QF0lO8LEpefoAmeoycv9F7PEXB6g.Z2qgb8djavHWKDmTS');
