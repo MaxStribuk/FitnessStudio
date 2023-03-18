c0111e2320a240ac771772a316bcc29f08e30c27
MaxStribuk <maximus_747@mail.ru>
2023-03-18 16:03:52 +0300

    refactoring

--- fitnesStudioModul/src/test/java/by/itacademy/FitnessStudio/FitnessStudioApplicationTests.java
+++ fitnesStudioModul/src/test/java/by/itacademy/FitnessStudio/FitnessStudioApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FitnessStudioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 }
