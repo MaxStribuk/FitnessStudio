ee208a226bd3b83b4fb5ba8c7e1d84635fb27dd9
MaxStribuk <maximus_747@mail.ru>
2023-03-18 14:34:33 +0300

    add auditRepository

---
+++ src/main/java/by/itacademy/repository/api/IAuditRepository.java
@@ -0,0 +1,18 @@
+package by.itacademy.repository.api;
+
+import by.itacademy.repository.entity.AuditEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IAuditRepository extends Repository<AuditEntity, UUID> {
+
+    <S extends AuditEntity> S save(S entity);
+
+    Optional<AuditEntity> findById(UUID uuid);
+
+    Page<AuditEntity> findAll(Pageable pageable);
+}
