dffa7811574a0550da388ca96b1c6e749ce70527
MaxStribuk <maximus_747@mail.ru>
2023-03-18 16:02:43 +0300

    added some modules

---
+++ postgresql/fitness_studio/dml/01.add_all.sql
@@ -0,0 +1,26 @@
+INSERT INTO app.user_role(role) VALUES ('USER');
+INSERT INTO app.user_role(role) VALUES ('ADMIN');
+
+INSERT INTO app.user_status(status) VALUES ('ACTIVATED');
+INSERT INTO app.user_status(status) VALUES ('DEACTIVATED');
+INSERT INTO app.user_status(status) VALUES ('WAITING_ACTIVATION');
+
+INSERT INTO app.mail_status(status) VALUES ('WAITING');
+INSERT INTO app.mail_status(status) VALUES ('SENT');
+INSERT INTO app.mail_status(status) VALUES ('ERROR');
+INSERT INTO app.mail_status(status) VALUES ('SUCCESS');
+
+INSERT INTO app.users
+VALUES ('00000000-0000-0000-0000-000000000000',
+        '2023-01-01 00:00:00.000',
+        '2023-01-01 00:00:00.000',
+        'maxstr@mail.ru',
+        'Max',
+        2,
+        1,
+        '$2a$10$WzPoFH6QF0lO8LEpefoAmeoycv9F7PEXB6g.Z2qgb8djavHWKDmTS');
+
+INSERT INTO app.essence_type(type) VALUES ('PRODUCT');
+INSERT INTO app.essence_type(type) VALUES ('RECIPE');
+INSERT INTO app.essence_type(type) VALUES ('USER');
+INSERT INTO app.essence_type(type) VALUES ('REPORT');
