aec7352b0f538114cc5ec834aa8edc48e0d20715
MaxStribuk <maximus_747@mail.ru>
2023-04-05 17:42:09 +0300

    ftp changed to google drive

--- fitnesStudioModul/src/main/java/by/itacademy/service/util/tasks/FileUploader.java
+++ fitnesStudioModul/src/main/java/by/itacademy/service/util/tasks/FileUploader.java
@@ -6,35 +6,32 @@ import by.itacademy.repository.entity.ReportEntity;
 import by.itacademy.repository.entity.ReportStatusEntity;
 import by.itacademy.service.api.IAuditService;
 import by.itacademy.service.api.IExcelFileWriter;
+import by.itacademy.service.api.IFileHandlingService;
 import by.itacademy.service.api.IReportService;
-import org.apache.commons.net.ftp.FTPFile;
-import org.springframework.integration.file.remote.session.Session;
-import org.springframework.integration.file.remote.session.SessionFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
 @Component
-public class FtpFileUploader implements Runnable {
+public class FileUploader implements Runnable {
 
     private final ScheduledExecutorService executorService;
     private final IReportService reportService;
     private final IAuditService auditService;
     private final IExcelFileWriter fileWriter;
-    private final SessionFactory<FTPFile> sessionFactory;
+    private final IFileHandlingService fileHandlingService;
 
-    public FtpFileUploader(ScheduledExecutorService executorService,
-                           IReportService reportService,
-                           IAuditService auditService,
-                           IExcelFileWriter fileWriter,
-                           SessionFactory<FTPFile> sessionFactory) {
+    public FileUploader(ScheduledExecutorService executorService,
+                        IReportService reportService,
+                        IAuditService auditService,
+                        IExcelFileWriter fileWriter,
+                        IFileHandlingService fileHandlingService) {
         this.executorService = executorService;
         this.reportService = reportService;
         this.auditService = auditService;
         this.fileWriter = fileWriter;
-        this.sessionFactory = sessionFactory;
+        this.fileHandlingService = fileHandlingService;
     }
 
     @Override
@@ -48,14 +45,10 @@ public class FtpFileUploader implements Runnable {
                 List<AuditEntity> audits = auditService.getForReport(report);
                 try {
                     byte[] bytes = fileWriter.write(audits);
-                    try (Session<FTPFile> session = sessionFactory.getSession()) {
-                        session.write(new ByteArrayInputStream(bytes),
-                                report.getUuid().toString() + ".xlsx");
-                    }
+                    fileHandlingService.upload(report.getUuid().toString(), bytes);
                     reportEntity.setStatus(new ReportStatusEntity(ReportStatus.DONE));
                 } catch (Exception e) {
                     reportEntity.setStatus(new ReportStatusEntity(ReportStatus.ERROR));
-                    e.printStackTrace();
                 } finally {
                     reportService.add(reportEntity);
                 }
